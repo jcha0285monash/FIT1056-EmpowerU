4e191f5ea0b27b754edb45268db6fcedde70e3cd
jcha0285monash <jcha0285@student.monash.edu>
2024-10-04 20:31:04 +0800

    Delete FIT1056 Final Delivarable 0.1 directory

--- FIT1056 Final Delivarable 0.1/app/user.py
+++
@@ -1,6 +0,0 @@
-class User:
-    def __init__(self, uid, email, password, name):
-        self.uid = uid
-        self.email = email
-        self.password = password
-        self.name = name
--- FIT1056 Final Delivarable 0.1/main_ui.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from interface.main_window import EmpowerU
-
-def main():
-    root = EmpowerU(title="EmpowerU", width=1000, height=600)
-    root.mainloop()
-
-if __name__ == '__main__':
-    main()
