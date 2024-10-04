878d8cd56466b515f14f2dffacbfd784ade6c6d5
jcha0285monash <jcha0285@student.monash.edu>
2024-10-04 20:23:47 +0800

    Add files via upload

---
+++ pst4_app_user.py
@@ -0,0 +1,21 @@
+"""
+FIT1056 2024 Semester 2
+Programming Concepts Task 4
+
+This file contains the class definition for the User class.
+DO NOT MODIFY THIS FILE!
+"""
+
+class User:
+
+    def __init__(self, uid, first_name, last_name, contact_num):
+        """
+        Constructor for the User class.
+        """
+        self.uid = uid
+        self.first_name = first_name
+        self.last_name = last_name
+        self.contact_num = contact_num
+
+if __name__ == "__main__":
+    pass
