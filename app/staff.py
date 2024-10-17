fec5751522aec0b9f21b9fb22ca74ac6e96aa011
jcha0285monash <jcha0285@student.monash.edu>
2024-10-17 17:53:51 +0800

    fixed auth for teacher and staff class

--- app/staff.py
+++ app/staff.py
@@ -2,7 +2,6 @@ from app.user import User
 import os
 
 class Staff(User):
-
     @staticmethod
     def authenticate(uid, password):
         staff_path = "./database/staff.txt"
@@ -11,7 +10,7 @@ class Staff(User):
                 lines = rf.readlines()
             for line in lines:
                 sta_id, email, stored_password, name, role, status = line.strip().split(",")
-                if uid == sta_id and password == stored_password and status != "DEACTIVATED":
+                if uid == sta_id and password == stored_password and (status != "DEACTIVATED" or status == None):
                     if "sta" in sta_id.lower():
                         return Staff(sta_id, email, password, name, role, status)
                     else:
