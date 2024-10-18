29e9fb4a3c7520292a59baa440f3f70e37ce87ba
Joelw <joelwsy-pm22@student.tarc.edu.my>
2024-10-18 20:44:08 +0800

    ..

--- test/functional_testing.py
+++ test/functional_testing.py
@@ -1,11 +1,18 @@
 import sys
-sys.path.append("..")
+import os
+
 import pytest
+
 from app.staff import Staff
 from app.student import Student
 from app.teacher import Teacher
 
 def test_authenticate():
+    # try logging on with correct credentials
+    test_authenticate = Student.authenticate("stu0001", "pass")
+    assert type(test_authenticate) == Student
+    assert test_authenticate.name == "Joel"
+    assert test_authenticate.email == "joel@monash.edu"
     
     # try logging on with incorrect credentials
     test_authenticate = Student.authenticate("stu123", "password123")
@@ -13,3 +20,5 @@ def test_authenticate():
     test_authenticate = Teacher.authenticate("tea123", "password123")
     assert test_authenticate == None
     test_authenticate = Staff.authenticate("sta123", "password123")
+    assert test_authenticate == None
+test_authenticate()
