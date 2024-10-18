43b7915741aaeedd27a26ea11c50e5bc22698fca
Joelw <joelwsy-pm22@student.tarc.edu.my>
2024-10-18 17:19:29 +0800

    added details for correct login methods

--- test/functional_testing.py
+++ test/functional_testing.py
@@ -1,8 +1,15 @@
-import pytest 
-from app import user, student, teacher, staff
+import sys
+sys.path.append("..")
+import pytest
+from app.staff import Staff
+from app.student import Student
+from app.teacher import Teacher
 
 def test_authenticate():
-    test_authenticate = student.authenticate("stu123", "password123")
+    
+    # try logging on with incorrect credentials
+    test_authenticate = Student.authenticate("stu123", "password123")
     assert test_authenticate == None
-    test_authenticate = teacher.authenticate("tea123", "password123")
+    test_authenticate = Teacher.authenticate("tea123", "password123")
     assert test_authenticate == None
+    test_authenticate = Staff.authenticate("sta123", "password123")
