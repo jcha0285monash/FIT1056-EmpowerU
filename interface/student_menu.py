96571feb682987208c38fb5ea9b8c73f312dee35
darrickcheats <130717358+darrickcheats@users.noreply.github.com>
2024-10-19 13:13:19 +0800

    Add files via upload

--- interface/student_menu.py
+++ interface/student_menu.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from interface.student_menus.available_courses_menu import AvailableCoursesMenu
+from interface.student_menus.course_content import CourseDetailsPage
 
 class StudentMenu(tk.Frame):
     def __init__(self, master, student_user):
@@ -29,6 +30,10 @@ class StudentMenu(tk.Frame):
         self.alert_label = tk.Label(self, textvariable=self.alert_var, fg="red", font=("Arial", 12))
         self.alert_label.pack(pady=10)
 
+        # View course content button
+        self.view_course_button = tk.Button(self, text="View Course Content", command=self.view_course_content)
+        self.view_course_button.pack(pady=10)
+
         #logout button
         self.logout_button = tk.Button(self, text="Logout", command=self.logout_button_clicked)
         self.logout_button.pack(pady=10)
@@ -48,3 +53,16 @@ class StudentMenu(tk.Frame):
         self.master.hide_student_menu(self)
         available_courses_menu = AvailableCoursesMenu(self.master, self.student_user, self)
         self.master.show_available_courses_menu(available_courses_menu)
+
+    def view_course_content(self):
+        selected_course = self.courses_list.get(tk.ACTIVE)
+        if selected_course:
+            # Transition to C_homepage with the selected course
+            self.master.hide_student_menu(self)
+            
+            # Create the course homepage, passing the selected course
+            course_details_page = CourseDetailsPage(self.master, selected_course, self.student_user, self)
+            course_details_page.pack(fill="both", expand=True) 
+        else:
+            # Show an alert if no course is selected
+            self.alert_var.set("Please select a course to view its content.")
