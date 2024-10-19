c80c829464b398ba91fe771b159f8f557e641a99
Joelw <joelwsy-pm22@student.tarc.edu.my>
2024-10-19 13:14:58 +0800

    staff view teaccher

--- interface/staff_menus/staff_teacher_menu.py
+++ interface/staff_menus/staff_teacher_menu.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import os
 
 class StaffTeacherMenu(tk.Frame):
@@ -12,9 +13,26 @@ class StaffTeacherMenu(tk.Frame):
         self.heading = tk.Label(self, text="All Teachers", font=("Arial", 18, "bold"))
         self.heading.pack(pady=20)
 
-        # teacher listbox
-        self.teacher_listbox = tk.Listbox(self, width=50, height=10)
-        self.teacher_listbox.pack(pady=20)
+        # create tree view
+        self.tree = ttk.Treeview(self, columns=("UID", "Email", "Password", "Name", "Course","Status"), show="headings")
+        self.tree.heading("UID", text="UID")
+        self.tree.heading("Email", text="Email")
+        self.tree.heading("Password", text="Password")
+        self.tree.heading("Name", text="Name")
+        self.tree.heading("Course", text="Course")
+        self.tree.heading("Status", text="Status")
+
+        # define column width and alignment
+        self.tree.column("UID", width=80, anchor=tk.CENTER)
+        self.tree.column("Email", width=150, anchor=tk.W)
+        self.tree.column("Password", width=100, anchor=tk.W)
+        self.tree.column("Name", width=150, anchor=tk.W)
+        self.tree.column("Course", width=150, anchor=tk.W)
+        self.tree.column("Status", width=100, anchor=tk.W)
+        
+        # position the Treeview
+        self.tree.pack(pady=1)
+        
         self.load_teachers()
 
         # alert variable and label widget
@@ -31,13 +49,15 @@ class StaffTeacherMenu(tk.Frame):
         self.back_to_home_button.pack(pady=10)
 
     def load_teachers(self):
-        self.teacher_listbox.delete(0, tk.END)
-        all_students_path = "database/teacher.txt"
-        if os.path.exists(all_students_path):
-            with open(all_students_path, "r", encoding="utf-8") as rf:
+        for item in self.tree.get_children():
+            self.tree.delete(item)
+        all_teachers_path = "./database/teacher.txt"
+        if os.path.exists(all_teachers_path):
+            with open(all_teachers_path, "r", encoding="utf-8") as rf:
                 lines = rf.readlines()
             for line in lines:
-                self.teacher_listbox.insert(tk.END, line.strip())
+                values = line.strip().split(",")
+                self.tree.insert("", tk.END, values=values)
 
     def edit_teacher_button_clicked(self):
         # todo
